5e7569a8850be87607484f2df843bbdf7923cb4e
José Gama <jose.gama@ccc.ufcg.edu.br>
2021-04-20 11:52:04 -0300

    add pegaVacina e funcoes de listagem por atributo

--- Haskell/Vacina.hs
+++ Haskell/Vacina.hs
@@ -24,4 +24,38 @@ todasAsVacinas (h:t) = "Nome: " ++ nome h ++ "Data de fabricação: " ++ dataFab
         ++ "Estoque: " ++ show (estoque h) ++ " Quantidade de doses necessarias: " ++ show (quantidadeDosesNecessarias h) ++ " " ++
         "Enfermidade: " ++ enfermidade h++ " "++ "Taxa de eficiência" ++ show (taxaEficiencia h) ++ 
         " "  ++ "Selo de aprovação: " ++ seloAprovacao h ++ "Pais de origem: "++ paisOrigem h ++ "\n" ++ todasAsVacinas t
-        
+
+pegaVacina :: String -> [Vacina] -> Maybe Vacina
+pegaVacina _ [] = Nothing
+pegaVacina nomeVacina (h:t)
+    | nome h == nomeVacina = Just h
+    | otherwise = pegaVacina nomeVacina t
+
+vacinasPorLaboratorio :: String -> [Vacina] -> String
+vacinasPorLaboratorio _ [] = ""
+vacinasPorLaboratorio nomeLaboratorio (h:t)
+    | laboratorio h == nomeLaboratorio = "Nome: " ++ nome h ++ "Data de fabricação: " ++ dataFabricacao h ++
+        " " ++ "Data de validade: " ++ dataValidade h ++ " " ++ " "
+        ++ "Estoque: " ++ show (estoque h) ++ " Quantidade de doses necessarias: " ++ show (quantidadeDosesNecessarias h) ++ " " ++
+        "Enfermidade: " ++ enfermidade h++ " "++ "Taxa de eficiência" ++ show (taxaEficiencia h) ++ 
+        " "  ++ "Selo de aprovação: " ++ seloAprovacao h ++ "Pais de origem: "++ paisOrigem h ++ "\n" ++ vacinasPorLaboratorio nomeLaboratorio t
+    | otherwise = vacinasPorLaboratorio nomeLaboratorio t
+
+vacinasPorEnfermidade :: String -> [Vacina] -> String
+vacinasPorEnfermidade _ [] = ""
+vacinasPorEnfermidade nomeEnfermidade (h:t)
+    | laboratorio h == nomeEnfermidade = "Nome: " ++ nome h ++ "Data de fabricação: " ++ dataFabricacao h ++
+        " " ++ "Data de validade: " ++ dataValidade h ++ " " ++ "Laboratório: " ++ laboratorio h ++ " "
+        ++ "Estoque: " ++ show (estoque h) ++ " Quantidade de doses necessarias: " ++ show (quantidadeDosesNecessarias h) ++ " " ++ "Taxa de eficiência" ++ show (taxaEficiencia h) ++ 
+        " "  ++ "Selo de aprovação: " ++ seloAprovacao h ++ "Pais de origem: "++ paisOrigem h ++ "\n" ++ vacinasPorEnfermidade nomeEnfermidade t
+    | otherwise = vacinasPorEnfermidade nomeEnfermidade t
+
+vacinasPorPaisDeOrigem :: String -> [Vacina] -> String
+vacinasPorPaisDeOrigem _ [] = ""
+vacinasPorPaisDeOrigem nomePaisDeOrigem (h:t)
+    | laboratorio h == nomePaisDeOrigem = "Nome: " ++ nome h ++ "Data de fabricação: " ++ dataFabricacao h ++
+        " " ++ "Data de validade: " ++ dataValidade h ++ " " ++ "Laboratório: " ++ laboratorio h ++ " "
+        ++ "Estoque: " ++ show (estoque h) ++ " Quantidade de doses necessarias: " ++ show (quantidadeDosesNecessarias h) ++ " " ++
+        "Enfermidade: " ++ enfermidade h++ " "++ "Taxa de eficiência" ++ show (taxaEficiencia h) ++ 
+        " "  ++ "Selo de aprovação: " ++ seloAprovacao h ++ vacinasPorPaisDeOrigem nomePaisDeOrigem t
+    | otherwise = vacinasPorPaisDeOrigem nomePaisDeOrigem t
