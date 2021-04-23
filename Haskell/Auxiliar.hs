95edc55c51c17d5da64bc86951aa6890e68fa29a
José Gama <jose.gama@ccc.ufcg.edu.br>
2021-04-23 11:38:56 -0300

    adicionei metodos para o agendamento de vacinacoes

--- Haskell/Auxiliar.hs
+++ Haskell/Auxiliar.hs
@@ -2,6 +2,7 @@ module Auxiliar where
 
 import qualified Vacina
 import qualified Paciente
+import qualified Vacinacao
 import Data.Map as Map (fromList, Map)
 import Data.List
 import Data.List.Split (splitOn)
@@ -26,6 +27,13 @@ iniciaPaciente = do
     let lista_paciente = Data.List.map constroiPaciente lista
     return lista_paciente
 
+iniciaVacinacao:: IO[Vacinacao.Vacinacao]
+iniciaVacinacao = do
+    arquivo <- Strict.readFile "dados/vacinações.txt"
+    let lista = map (splitOn ",") (lines arquivo)
+    let lista_vacinacoes = Data.List.map constroiVacinacao lista
+    return lista_vacinacoes
+
 escreverVacina:: Vacina.Vacina -> IO()
 escreverVacina vacina = do
     let vacinaStr = Vacina.nome vacina ++ "," ++ show (Vacina.dataFabricacao vacina) ++ "," ++ show (Vacina.dataValidade vacina) ++ "," ++ Vacina.laboratorio vacina ++ "," ++ show (Vacina.estoque vacina) ++ "," ++ show (Vacina.quantidadeDosesNecessarias vacina) ++ "," ++ Vacina.enfermidade vacina ++ "," ++ show (Vacina.taxaEficiencia vacina) ++ "," ++ Vacina.seloAprovacao vacina ++ "," ++ Vacina.paisOrigem vacina ++ "\n"
@@ -38,6 +46,19 @@ escreverPaciente paciente = do
     let pacienteStr = Paciente.nome paciente ++ "," ++ show (Paciente.sexo paciente) ++ "," ++ show (Paciente.cpf paciente) ++ "," ++ show (Paciente.cep paciente) ++ "," ++ Paciente.bairro paciente ++ "," ++ Paciente.rua paciente ++ "," ++ show(Paciente.numResidencia paciente) ++ "," ++ Paciente.dataNascimento paciente ++ "," ++ Paciente.telefone paciente ++ "," ++ "\n"
     appendFile "dados/pacientes.txt" pacienteStr
     return ()
+
+escreverVacinacao :: Vacinacao.Vacinacao -> IO()
+escreverVacinacao vacinacao = do
+    let vacinacaoStr = Vacinacao.nomeVacina vacinacao ++ "," ++
+                    Vacinacao.local vacinacao ++ "," ++
+                    Vacinacao.dataPrimeiraDose vacinacao ++ "," ++
+                    Vacinacao.dataSegundaDose vacinacao ++ "," ++
+                    Vacinacao.horarioInicio vacinacao ++ "," ++
+                    Vacinacao.horarioFim vacinacao ++ "," ++
+                    show (Vacinacao.faixaEtariaInicio vacinacao) ++ "," ++
+                    show (Vacinacao.faixaEtariaFim vacinacao) ++ "," ++ "\n"
+    appendFile "dados/vacinações.txt" vacinacaoStr
+    return()
 
 constroiVacina :: [String] -> Vacina.Vacina 
 constroiVacina lista = 
@@ -66,4 +87,17 @@ constroiPaciente lista =
         Paciente.numResidencia =  read (lista !! 6),
         Paciente.dataNascimento = read (lista !! 7),
         Paciente.telefone =  lista !! 8
+    }
+
+constroiVacinacao :: [String] -> Vacinacao.Vacinacao
+constroiVacinacao lista =
+    Vacinacao.Vacinacao {
+        Vacinacao.nomeVacina = lista !! 0,
+        Vacinacao.local = lista !! 1,
+        Vacinacao.dataPrimeiraDose = lista !! 2,
+        Vacinacao.dataSegundaDose  = lista !! 3,
+        Vacinacao.horarioInicio  = lista !! 4,
+        Vacinacao.horarioFim  = lista !! 5,
+        Vacinacao.faixaEtariaInicio = read (lista !! 6),
+        Vacinacao.faixaEtariaFim  = read (lista !! 7)
     }
