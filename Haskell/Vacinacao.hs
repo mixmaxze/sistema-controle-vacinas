8467410621722e3c893bb44005c5122cd77c62d2
José Gama <jose.gama@ccc.ufcg.edu.br>
2021-04-23 11:39:42 -0300

    criei modulo e tipo Vacinacao

---
+++ Haskell/Vacinacao.hs
@@ -0,0 +1,16 @@
+module Vacinacao where
+
+data Vacinacao = Vacinacao {nomeVacina :: String, local :: String, dataPrimeiraDose :: String, dataSegundaDose :: String, horarioInicio :: String, horarioFim :: String, faixaEtariaInicio :: Int, faixaEtariaFim :: Int} deriving(Show, Eq)
+
+adicionaVacinacao :: String -> String -> String -> String -> String -> String -> Int -> Int -> Vacinacao
+adicionaVacinacao nomeVacina local dataPrimeiraDose dataSegundaDose horarioInicio horarioFim faixaEtariaInicio faixaEtariaFim =
+     (Vacinacao {
+         nomeVacina = nomeVacina,
+         local = local,
+         dataPrimeiraDose = dataPrimeiraDose,
+         dataSegundaDose = dataSegundaDose,
+         horarioInicio = horarioInicio,
+         horarioFim = horarioFim,
+         faixaEtariaInicio = faixaEtariaInicio,
+         faixaEtariaFim = faixaEtariaFim
+     })
