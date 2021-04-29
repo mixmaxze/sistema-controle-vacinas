c6a67d29630cde8c98e7b009e24dad0aac264a2e
José Gama <jose.gama@ccc.ufcg.edu.br>
2021-04-29 19:51:56 -0300

    add metodos de atualizacoes de atributos

--- Haskell/Paciente.hs
+++ Haskell/Paciente.hs
@@ -1,5 +1,7 @@
 module Paciente where
 
+import Data.List
+
 data Paciente = Paciente {nome:: String, sexo:: String, cpf::String, endereco:: String, idade:: Int, telefone::String} deriving(Show, Eq)
 
 adicionaPaciente:: String -> String -> String -> String -> Int -> String -> Paciente
@@ -29,12 +31,38 @@ pegaIdadePaciente cpfPaciente (h:t)
     | cpf h == cpfPaciente = idade h
     |otherwise = pegaIdadePaciente cpfPaciente t
 
-contaPaciente :: [Paciente] -> Int
-contaPaciente [] = 0
-contaPaciente lista = length (lista)
-
 pegaPacientesHabilitados :: Int -> [Paciente] -> String
 pegaPacientesHabilitados _ [] = ""
 pegaPacientesHabilitados faixaEtaria (h:t)
                         | idade h >= faixaEtaria = "Nome: | " ++ nome h ++ " | Telefone: | " ++ telefone h ++ " | Sexo: " ++ sexo h ++ "\n" ++ pegaPacientesHabilitados faixaEtaria t ++ "\n"
                         | otherwise = pegaPacientesHabilitados faixaEtaria t ++ "\n"
+
+atualizaEnderecoPaciente :: String -> [Paciente] -> String -> [Paciente]
+atualizaEnderecoPaciente cpfPaciente [] novoEndereco = []
+atualizaEnderecoPaciente cpfPaciente (h:t) novoEndereco 
+    | isInfixOf (cpfPaciente) (cpf h) == True = ((Paciente(nome h) (sexo h) (cpf h) (novoEndereco) (idade h) (telefone h)) : t)
+    | otherwise =  (h: atualizaEnderecoPaciente cpfPaciente t novoEndereco)
+
+atualizaNomePaciente :: String -> [Paciente] -> String -> [Paciente]
+atualizaNomePaciente cpfPaciente [] novoValor = []
+atualizaNomePaciente cpfPaciente (h:t) novoValor 
+    | isInfixOf (cpfPaciente) (cpf h) == True = ((Paciente(novoValor) (sexo h) (cpf h) (endereco h) (idade h) (telefone h)) : t)
+    | otherwise =  (h: atualizaNomePaciente cpfPaciente t novoValor)
+
+atualizaSexoPaciente :: String -> [Paciente] -> String -> [Paciente]
+atualizaSexoPaciente cpfPaciente [] novoValor = []
+atualizaSexoPaciente cpfPaciente (h:t) novoValor 
+    | isInfixOf (cpfPaciente) (cpf h) == True = ((Paciente(nome h) (novoValor) (cpf h) (endereco h) (idade h) (telefone h)) : t)
+    | otherwise =  (h: atualizaSexoPaciente cpfPaciente t novoValor)
+
+atualizaTelefonePaciente :: String -> [Paciente] -> String -> [Paciente]
+atualizaTelefonePaciente cpfPaciente [] novoValor = []
+atualizaTelefonePaciente cpfPaciente (h:t) novoValor 
+    | isInfixOf (cpfPaciente) (cpf h) == True = ((Paciente(nome h) (sexo h) (cpf h) (endereco h) (idade h) (novoValor)) : t)
+    | otherwise =  (h: atualizaTelefonePaciente cpfPaciente t novoValor)
+
+atualizaIdadePaciente :: String -> [Paciente] -> Int -> [Paciente]
+atualizaIdadePaciente cpfPaciente [] novoValor = []
+atualizaIdadePaciente cpfPaciente (h:t) novoValor 
+    | isInfixOf (cpfPaciente) (cpf h) == True = ((Paciente(nome h) (sexo h) (cpf h) (endereco h) (novoValor) (telefone h)) : t)
+    | otherwise =  (h: atualizaIdadePaciente cpfPaciente t novoValor)
