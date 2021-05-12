b7d0d922405b4ce841510f23e10d022e6258753b
José Gama <jose.gama@ccc.ufcg.edu.br>
2021-05-12 15:54:50 -0300

    corrigido sequencia de fatos

--- Prolog/Vacina.pl
+++ Prolog/Vacina.pl
@@ -6,14 +6,14 @@ constroiVacina(Nome,Fabricacao,Validade,Laboratorio,Quantidade,QuantidadeDoses,D
 %salvaVacina(Vacina,Lista,Result):- append(Lista,[Vacina],Result).
 
 /*get dados de Vacina*/
-getVacinaNome(Nome,_,_,_,_,_,_,_,_,_),Nome).
-getVacinaFabricacao(_,Fabricacao,_,_,_,_,_,_,_,_),Fabricacao).
-getVacinaValidade(_,_,Validade,_,_,_,_,_,_,_),Validade).
-getVacinaLaboratorio(_,_,_,Laboratorio,_,_,_,_,_,_),Laboratorio).
-getVacinaQuantidade(_,_,_,_,Quantidade,_,_,_,_,_),Quantidade).
-getVacinaQuantidadeDoses(_,_,_,_,_,QuantidadeDoses,_,_,_,_),QuantidadeDoses).
-getVacinaDoenca(_,_,_,_,_,_,Doenca,_,_,_),Doenca).
-getVacinaEficiencia(_,_,_,_,_,_,_,Eficiencia,_,_),Eficiencia).
-getVacinaSelo(_,_,_,_,_,_,_,_,Selo,_),Selo).
-getVacinaPais(_,_,_,_,_,_,_,_,_,Pais),Pais).
+getVacinaNome((Nome,_,_,_,_,_,_,_,_,_),Nome).
+getVacinaFabricacao((_,Fabricacao,_,_,_,_,_,_,_,_),Fabricacao).
+getVacinaValidade((_,_,Validade,_,_,_,_,_,_,_),Validade).
+getVacinaLaboratorio((_,_,_,Laboratorio,_,_,_,_,_,_),Laboratorio).
+getVacinaQuantidade((_,_,_,_,Quantidade,_,_,_,_,_),Quantidade).
+getVacinaQuantidadeDoses((_,_,_,_,_,QuantidadeDoses,_,_,_,_),QuantidadeDoses).
+getVacinaDoenca((_,_,_,_,_,_,Doenca,_,_,_),Doenca).
+getVacinaEficiencia((_,_,_,_,_,_,_,Eficiencia,_,_),Eficiencia).
+getVacinaSelo((_,_,_,_,_,_,_,_,Selo,_),Selo).
+getVacinaPais((_,_,_,_,_,_,_,_,_,Pais),Pais).
 
